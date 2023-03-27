ca1a30025fe3b72c174f50fa24cbf677a57aa48f
zlatan1487 <vitalik.karpukhin@list.ru>
2023-03-28 00:42:28 +0300

    add letter_upper function and docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_upper(value):
+    """ this str_upper from vitaliy """
+    return value.upper()
+
+
+def letter_upper(value):
+    """ this letter_upper from vitaliy """
     return value.upper()
